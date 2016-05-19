20105505d217d8d0cef7dbf86ef15fc37c94c7b7
soporte SDGS. Digesi <ft.soporte@TPP.UPEU.EDU.PE>
2016-05-19 09:03:32 -0500

    js

--- js/miJS.js
+++ js/miJS.js
@@ -1,1 +1,24 @@
-alert("hola mundo js");
+var area = 0;
+var nombre = "luis";
+var bandera = true;
+
+//alert(nombre);
+
+function areaRectangulo(base, altura) {
+    area = base * altura;
+    document.getElementById("alert").innerHTML="<h2>El area es:<span style='color:red'>"+area+"</span></h2>";
+
+}
+
+function suma(a,b){
+    var total = a+b;
+    return total;
+}
+
+function total(){
+    alert("la suma es: "+suma(20,5));
+}
+
+function obtenerDatos(id,name,value){
+    alert("id: "+id+"\nName: "+name+"\nValue: "+value)
+}
